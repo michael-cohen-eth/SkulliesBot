21d90425bc92ecc5c17aa09127a8cc11c71cd6a8
Michael Cohen <michael.cohen@opensea.io>
2021-12-28 17:59:37 -0500

    move around queue

--- clock.py
+++ clock.py
@@ -1,26 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 
-from post import do_tweets
-from queue import q
+from queue import enqueue_job
 
 sched = BackgroundScheduler(timezone=utc)
 
 @sched.scheduled_job('interval', hours=2)
 def timed_job():
 	print('Enqueuing tweet job...')
-	q.enqueue(publish_tweets_job)
-
-def publish_tweets_job():
-	if not get_is_enabled():
-		print("Scheduler disabled...")
-		return
-	event_strings = do_tweets()
-	if len(event_strings) > 0:
-		for event in event_strings:
-			print(event)
+	if get_is_enabled():
+		enqueue_job()
 	else:
-		print("No new events. Sleeping...")
+		print("Scheduler disabled...")
 
 def get_is_enabled() -> bool:
 	return sched.running
